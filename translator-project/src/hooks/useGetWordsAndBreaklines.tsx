dc6c1cc165a777c8d150c93dc41ad72bb4b1e840
Efaideleon <109036920+Efai-De-Leon@users.noreply.github.com>
2024-01-03 14:39:46 -0800

    adding documentation

--- translator-project/src/hooks/useGetWordsAndBreaklines.tsx
+++ translator-project/src/hooks/useGetWordsAndBreaklines.tsx
@@ -8,18 +8,24 @@ const useGetWordsAndBreaklines = (inputText: string): {
         throw new Error("inputText is undefined")
     }
 
+    // languages to display
     const spanishAndEnglishWordPairs: string[] = inputText.split('\n')
     const englishWords: string[] = []
     const spanishWords: string[] = []
     const phoneticWords: string[] = []
     const breaklines: boolean[] = []
 
-    spanishAndEnglishWordPairs.forEach(wordPair => {
-        const [englishWord = '', spanishWord = ''] = wordPair.split("|")
-        englishWords.push(englishWord || '')
-        spanishWords.push(spanishWord || '')
-        breaklines.push(false)
-    })
+    // split input text into their respecitve language
+    try{
+        spanishAndEnglishWordPairs.forEach(wordPair => {
+            const [englishWord = '', spanishWord = ''] = wordPair.split("|")
+            englishWords.push(englishWord || '')
+            spanishWords.push(spanishWord || '')
+            breaklines.push(false)
+        })
+    } catch(error) {
+        console.error("Error splitting wordPair with | ", error)
+    }
 
     return ({ english: englishWords, spanish: spanishWords, phonetic: phoneticWords, breaklines: breaklines })
 }
